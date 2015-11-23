3f60a31c52eb33530bc3de7990753eaee60afe22
anilvinnakota <anil.vinnakota@gmail.com>
2015-11-24 00:46:58 +0530

    added actor table

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
+Actors = new Mongo.Collection("actors");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Template.tagger.events({
+    'click .js-form': function(event) {
+      $("#actor_add_form").modal('show');
+    }, 
   });
+  Template.actor_add_form.events({
+    'submit .js-add-image':function(event){
+      var img_src, img_alt;
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+        img_src = event.target.img_src.value;
+        img_alt = event.target.img_alt.value;
+        console.log("src: "+img_src+" alt:"+img_alt);
+        if (Meteor.user()){
+          Actors.insert({
+            img_src:img_src, 
+            img_alt:img_alt, 
+            createdOn:new Date(),
+            createdBy:Meteor.user()._id
+          });
+        }
+        $("#actor_add_form").modal('hide');
+     return false;
     }
   });
 }
@@ -19,5 +33,17 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+            if (Actors.find().count() == 0){
+                for (var i=1;i<23;i++){
+                        Actors.insert(
+                                {
+                                        img_src:"img_"+i+".jpg",
+                                        img_alt:"image number "+i
+                                }
+                        );
+                }// end of for insert images
+                // count the images!
+                console.log("startup.js says: "+Actors.find().count());
+        }// end of if have no images
   });
 }
--- collections.js
+++
@@ -1,1 +0,0 @@
-aList = new Mongo.Collection('actors');
